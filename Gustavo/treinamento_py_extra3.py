fa4b005bd413bb4d870df8ebe8d92f0f08c7b209
Gustavo Leal <89709614+GustavoLealM@users.noreply.github.com>
2025-05-25 21:34:30 -0300

    Add files via upload

---
+++ Gustavo/treinamento_py_extra3.py
@@ -0,0 +1,34 @@
+# Autoria:Gustavo Leal Malafaia
+# Data:25/05/2025
+# Objetivo do progama:Exemplificar encapsulamento
+
+class BaseDeDados:
+
+    def __init__(self):
+        self.__dados = {}
+
+    def inserir_clientes(self, id, nome):
+        if 'clientes' not in self.__dados:
+            self.__dados['clientes'] = {id: nome}
+        else:
+            self.__dados['clientes'].update({id: nome})
+
+    def lista_clientes(self):
+        for id, nome in self.__dados['clientes'].items():
+            print(id, nome)
+
+    def apaga_cliente(self, id):
+        del self.__dados['clientes'][id]
+
+    @property  # Método getter para ver os dados da variável encapsulada
+    def ver_dados(self):
+        return self.__dados.copy()
+
+
+b1 = BaseDeDados()
+
+b1.inserir_clientes(23, 'joca')
+
+b1.lista_clientes()
+
+print(b1.ver_dados)  # Acesso dos dados encapsulados
